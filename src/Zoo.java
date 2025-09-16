c3cef3814427c049b2d0a23060463a66b615e847
mohsennabli <mohsennabli321@gmail.com>
2025-09-16 12:13:52 +0100

    prosit inst5

---
+++ src/Zoo.java
@@ -0,0 +1,13 @@
+class Zoo {
+    Animal[] animals;
+    String name;
+    String city;
+    int nbrCages;
+
+    public Zoo(String name, String city, int nbrCages) {
+        this.name = name;
+        this.city = city;
+        this.nbrCages = nbrCages;
+        this.animals = new Animal[25];
+    }
+}
--- src/ZooManagement.java
+++ src/ZooManagement.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class ZooManagement {
     public static void main(String[] args) {
-        int nbrCages=20;
+       /* int nbrCages=20;
         String ZooName= "My Zoo";
         System.out.println("Zoo Name: "+ZooName + "nbr Cages: "+nbrCages);
         Scanner input = new Scanner(System.in) ;
@@ -11,6 +11,16 @@ public class ZooManagement {
 
         System.out.println("insert zoo age");
         nbrCages = input.nextInt();
-        System.out.println("Zoo Name: "+ZooName+ " nbr Cages: "+nbrCages);
+        System.out.println("Zoo Name: "+ZooName+ " nbr Cages: "+nbrCages);*/
+
+        Animal dog = new Animal("doberman", "daleji", 11, true);
+
+        Zoo zoo = new Zoo ("Belvedaire","Tunis",5);
+
+        System.out.println("Zoo Name"+zoo.name);
+
+
+
+
     }
 }
