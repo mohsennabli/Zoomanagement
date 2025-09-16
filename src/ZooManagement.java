fe2d02996b5eb69ca155ef5b74a85a9fa8382c8c
mohsennabli <mohsennabli321@gmail.com>
2025-09-16 11:40:07 +0100

    Zoo first

---
+++ src/ZooManagement.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ZooManagement {
+    public static void main(String[] args) {
+        int nbrCages=20;
+        String ZooName= "My Zoo";
+        System.out.println("Zoo Name: "+ZooName + "nbr Cages: "+nbrCages);
+        Scanner input = new Scanner(System.in) ;
+        System.out.println("insert zoo name");
+        ZooName = input.nextLine();
+
+        System.out.println("insert zoo age");
+        nbrCages = input.nextInt();
+        System.out.println("Zoo Name: "+ZooName+ " nbr Cages: "+nbrCages);
+    }
+}
